4e500f058cca4cf28368f0c9cd704587718b752a
sinhong jeong <jsh719803@gmail.com>
2024-02-26 06:22:50 +0900

    task : controller 틀 작성

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,14 @@
+export default class UserController {
+  // constructor() {
+  //   this.userService = userService;
+  // }
+
+  // 회원가입 라우터랑 연결할 컨트롤러 메서드
+  signup = async (req, res, next) => {};
+
+  // 로그인 라우터랑 연결할 컨트롤러 메서드
+  signin = async (req, res, next) => {};
+
+  // 프로필 조회 라우터랑 연결할 컨트롤러 메서드
+  getProfile = async (req, res, next) => {};
+}
