fb771e2d6ddfa943fc3c1bbe782f2c796e461ba6
sinhong jeong <jsh719803@gmail.com>
2024-02-26 07:54:39 +0900

    task : serivic 틀 작성하기

---
+++ src/services/users.service.js
@@ -0,0 +1,13 @@
+export default class UserService {
+  signup = async () => {
+    // signup 컨트롤러의 비즈니스 로직
+  };
+
+  signin = async () => {
+    // signin 컨트롤러의 비즈니스 로직
+  };
+
+  getProfile = async () => {
+    // getProfile 컨트롤러의 비즈니스 로직
+  };
+}
