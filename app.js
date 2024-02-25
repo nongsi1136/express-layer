9637d005c1db9a94a1ff2794a131721845454ce8
sinhong jeong <jsh719803@gmail.com>
2024-02-26 03:01:36 +0900

    task : app.js 파일 cookie-parser 등록

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
-import UsersRouter from "./routers/users.router.js";
+import cookieParser from "cookie-parser";
+import UsersRouter from "./src/routers/users.router.js";
 
 const app = express();
 const PORT = 3326;
 
+app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api", UsersRouter);
+app.use("/api", [UsersRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
