0002573a8f290358c6991c3feae8624c0595b898
sinhong jeong <jsh719803@gmail.com>
2024-02-26 02:31:55 +0900

    feat : UsersRouter 등록

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
+import UsersRouter from "./routers/users.router.js";
 
 const app = express();
 const PORT = 3326;
 
 app.use(express.json());
 
-app.use("/api");
+app.use("/api", UsersRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
