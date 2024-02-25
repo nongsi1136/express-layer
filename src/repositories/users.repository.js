1a21b1bf1e360a41828f5bbb2308d47e01e2cc32
sinhong jeong <jsh719803@gmail.com>
2024-02-26 07:57:02 +0900

    task: repository 파일 틀 작성

---
+++ src/repositories/users.repository.js
@@ -0,0 +1,12 @@
+export default class UserRepository {
+  // 회원가입, 로그인, 프로필 조회 api에서 프리즈마를 사용하던 부분을 떠올림
+  // email로 유저를 조회하거나
+  // userId로 유저를 조회하거나
+  // 유저를 생성하는 동작이 필요함
+
+  getUserByEmail = async () => {};
+
+  getUserById = async () => {};
+
+  createUser = async () => {};
+}
